ec62c32e5adf1cc3a3e2415a522fccecbe5c3f89
Rustam Kildeev <kildeevrustam02@gmail.com>
2024-03-12 20:52:51 +0100

    first commit

--- precode.go
+++ precode.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -40,13 +41,60 @@ var tasks = map[string]Task{
 }
 
 // Ниже напишите обработчики для каждого эндпоинта
-// ...
+func getAllTasks(w http.ResponseWriter, r *http.Request) { //для получения всех задач
+	jsonResp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError) // 500
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) //статус 200 OK
+	w.Write(jsonResp)
+}
+
+func getTaskID(w http.ResponseWriter, r *http.Request) { // для получения по id
+	taskId := chi.URLParam(r, "id")
+	task, ok := tasks[taskId]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest) // если не найдена 400
+		return
+	}
+	jsonResp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) //статус 200 OK
+	w.Write(jsonResp)
+}
+func createTasks(w http.ResponseWriter, r *http.Request) { // создание задач
+	var task Task
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest) // 400
+		return
+	}
+	tasks[task.ID] = task             // cоздаем задачу
+	w.WriteHeader(http.StatusCreated) // статус 201
+}
+func deleteTaskID(w http.ResponseWriter, r *http.Request) { // удаление задачи
+	taskId := chi.URLParam(r, "id") // значение id
+	_, ok := tasks[taskId]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest) // 400
+		return
+	}
+	delete(tasks, taskId) // удалить задачу
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
-	// ...
+	r.Get("/tasks", getAllTasks)    // обрабочик для получения всех задач
+	r.Post("/tasks", createTasks)   // создание задач
+	r.Get("/tasks/{id}", getTaskID) // обработчик доя задачи по ID
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
